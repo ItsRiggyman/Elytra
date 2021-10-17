0bdc479466523bd73776edc1380e97107304d1b7
Isaac Clowes <riggyman@tutanota.com>
2021-10-17 19:04:00 +0100

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.1.12"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
+    paperclip("io.papermc:paperclip:2.0.1")
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java { toolchain { languageVersion.set(JavaLanguageVersion.of(16)) } }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(16)
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":Elytra-Server"))
+
+    useStandardUpstream("airplane") {
+        remapRepo.set("https://maven.quiltmc.org/repository/release/")
+        decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+        url.set(github("TECHNOVE", "Airplane"))
+        ref.set(providers.gradleProperty("AirplaneCommit"))
+
+        withStandardPatcher {
+            baseName("Airplane")
+            
+            apiOutputDir.set(layout.projectDirectory.dir("Elytra-API"))
+            serverOutputDir.set(layout.projectDirectory.dir("Elytra-Server"))
+        }
+    }
+}
